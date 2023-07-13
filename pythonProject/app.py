6b0cb47441435eb785c1ba75b4b13b47ac0130fe
鲜帅 <2969280941@qq.com>
2023-07-13 15:25:06 +0800

    Stable Diffusion 文档补充

--- pythonProject/app.py
+++ pythonProject/app.py
@@ -1,133 +1,32 @@
-# # import time
-# # for i in range(100):
-# #     time.sleep(0.2)
-# #     print(f'\r\033[3{i%10};4{i%10}mThis is a test !\033[0m',end='',flush=True)
-# #     # print(f'\033[32mThis is a test !\033[0m', end='',flush=True)
-# #
-# #
-#
-# #!/usr/bin/env python
-# # -*- coding:utf-8 -*-
-# # Datatime:2022/7/31 20:21
-# # Filename:017 Python 流程控制之 if 判断.py
-# # Toolby: PyCharm
-# # 坚持
-#
-# # 变量
-# # 数据类型
-# # 输入输出
-# # 基本运算符
-#
-#
-# # if
-# '''
-# if 条件成立:
-#     可以干一些事情
-# '''
-# # name = input("请问冲过来的是什么（dog, girl)：")
-# # if name == 'girl':
-# #     print("亲爱的，我来了")
-# #
-# #
-# # print('滚一边去，狗')
-#
-# '''
-# if 条件成立：
-#     走这里
-# elif 条件成立：
-#     走这里
-# elif ……可以有很多个
-# '''
-#
-#
-# # name = input("请问冲过来的是什么（dog, girl,boy)：")
-# # if name == 'girl':
-# #     print("亲爱的，我来了")
-# # elif name == 'dog':
-# #     print("疯狗，走开")
-# # elif name == 'boy':
-# #     print("抱歉，不搞基")
-# #
-# #
-# # print('傻 x，让你输入 dog、girl、boy')
-#
-# '''
-# if 条件1成立：
-#     1走这里
-# elif 条件 2 成立：
-#     2走这里
-# elif 条件 3 成立：
-#     3 走这里
-# else：
-#     如果上述都不成立，走这里
-# '''
-#
-#
-# # name = input("请问冲过来的是什么（dog, girl,boy)：")
-# # if name == 'girl':
-# #     print("亲爱的，我来了")
-# # elif name == 'dog':
-# #     print("疯狗，走开")
-# # elif name == 'boy':
-# #     print("抱歉，不搞基")
-# # else:
-# #     print('傻 x，让你输入 dog、girl、boy')
-#
-#
-# name = input("请问冲过来的是什么（dog, girl,boy)：")
-# if name == 'girl':
-#     print("亲爱的，我来了")
-#
-# if name == 'dog':
-#     print("疯狗，走开")
-#
-# if name == 'boy':
-#     print("抱歉，不搞基")
-#
-#
-# '''
-# https://www.cnblogs.com/nickchen121/p/10737326.html
-# 3.1 练习1：成绩评判
-# 如果 成绩>=90，打印"优秀"
-# 如果 成绩>=80 并且 成绩<90，打印"良好"
-# 如果 成绩>=70 并且 成绩<80，打印"普通"
-# 其他情况：打印"差"
-# '''
-#
-#
-import base64
-import datetime
+from flask import Flask, render_template, request, jsonify, session, redirect
+import json
 import hashlib
+import datetime
+import secrets
 import os
 import string
-from random import random
-
 import requests
-from flask import Flask, render_template, request, json, jsonify, redirect, session
+import random
+import base64
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '123456'
+app.secret_key = secrets.token_hex(16)
 
 
 # 首页
 @app.route('/')
 def index():
-    # # return 'Hello, World'
-    # return render_template('./index.html')
-
     # 如果未登录，就跳转到 login
     if 'userinfo' not in session:
         return redirect('/login')
 
-    # 已登录，渲染模板
-    return render_template("./index.html")
+    # 如果已经登录，才展示模板文件
+    return render_template('index.html')
 
 
 # 登录
-# @app.route('/login')
-# def login():
-#     # return '登录成功'
-#     return render_template('./login.html')
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
@@ -162,65 +61,13 @@ def login():
 
 
 # 注册
-# @app.route('/register')
-# def register():
-#     # return '注册成功'
-#     return render_template('./register.html')
-# 图片列表
-# @app.route('/imglist')
-# def img_list():
-#     # # return '图片列表'
-#     # return render_template('./imglist.html')
-#     # 如果未登录，就跳转到 login
-#     if 'userinfo' not in session:
-#         return redirect('/login')
-#
-#     # 已登录，渲染模板
-#     return render_template('./imglist.html')
-#
-
-# 图片列表
-@app.route('/imglist')
-def imglist():
-    # 如果未登录，就跳转到 login
-    if 'userinfo' not in session:
-        return redirect('/login')
-
-    username = session['userinfo']
-
-    img_file_path = './data/imglist.json'
-
-    try:
-        with open(img_file_path, 'r') as f:
-            img_dict = json.load(f)
-
-        user_img_list = []
-        if img_dict and username in img_dict:
-            user_img_list = img_dict[username]
-
-    except Exception as e:
-        user_img_list = []
-
-    return render_template('imglist.html', user_img_list=user_img_list)
-
-
-# @app.route('/api/data')
-# def get_data():
-#     # 处理请求并返回响应数据
-#     data = {'name': 'John', 'age': 25}
-#     return jsonify(data)
-
-
-# 注册
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'GET':
         # 渲染 register.html 模板文件并返回
         return render_template('register.html')
     else:
-
         data = request.get_json()
-        print(data)
         username = data.get('username')
         password = data.get('password')
 
@@ -251,12 +98,32 @@ def logout():
     return redirect('/login')
 
 
-# 文生图的路由
-# @app.route('/generateimg', methods=['POST'])
-# def generate_img():
-#     data = request.get_json()
-#     print(data)
-#     return jsonify({'code': 1, 'message': '图片生成成功'})
+# 图片列表
+
+
+@app.route('/imglist')
+def imglist():
+    # 如果未登录，就跳转到 login
+    if 'userinfo' not in session:
+        return redirect('/login')
+
+    username = session['userinfo']
+
+    img_file_path = './data/imglist.json'
+
+    try:
+        with open(img_file_path, 'r') as f:
+            img_dict = json.load(f)
+
+        user_img_list = []
+        if img_dict and username in img_dict:
+            user_img_list = img_dict[username]
+
+    except Exception as e:
+        user_img_list = []
+
+    return render_template('imglist.html', user_img_list=user_img_list)
+
 
 # 文生图的路由
 @app.route('/generateimg', methods=['POST'])
@@ -282,6 +149,8 @@ def generate_img():
     # 保存图片并记录路径
     img_paths = []
 
+    # 保存用户图片列表
+    user_img_list = []
     for i, img_base64 in enumerate(result['images']):
         # 生成随机文件名
         filename = ''.join(random.choices(string.ascii_letters + string.digits, k=16)) + '.png'
@@ -292,14 +161,16 @@ def generate_img():
         # 记录路径
         img_paths.append(os.path.join('static', today, filename).replace('\\', '/'))
         # 添加图片信息到 user_img_list
-        user_img_list = []
-
         img_info = {'path': os.path.join('static', today, filename).replace('\\', '/'), 'date': today}
         user_img_list.append(img_info)
+
+    save_img_list(user_img_list)
 
     # 返回 JSON 数据
     return jsonify({'code': 1, 'message': '图片生成成功', 'img_paths': img_paths})
 
+
+# 保存用户的图片列表
 
 def save_img_list(user_img_list):
     # user_img_list = [{'path': 'static/2023-06-26/4', 'date': '2023-06-26'}, {'path': 'static/2023-06-26/2', 'date': '2023-06-26'}, {'path': '3', 'date': '2023-06-26'}]
@@ -314,8 +185,7 @@ def save_img_list(user_img_list):
         imglist = json.load(f)
 
     # 获取当前登录用户的用户名
-    # username = session.get('userinfo', {}).get('username')
-    username = 'user_002'
+    username = session['userinfo']
 
     # 如果 imglist 中已经存在该用户的数据，就将新的图片列表合并到该用户的数据中
     if username in imglist:
@@ -332,4 +202,4 @@ def save_img_list(user_img_list):
 
 
 if __name__ == '__main__':
-    app.run(port=9526, debug=True)
+    app.run(host='0.0.0.0', port=9526)
